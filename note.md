a35daa5e834ff0c09a94a1be2f50e90e18f139d6
AFeiYA <386665678@qq.com>
2022-06-02 23:02:52 +0800

    L138

--- note.md
+++ note.md
@@ -4,3 +4,5 @@
 - Start the server with `parcel index.html`
 
 
+# L138
+- [Object.assign built-in function](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign)
--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,5 @@ import {User} from './models/User';
 console.log("Running")
 const user = new User({name: "John", age: 30});
 console.log(user.get("name"));
+user.set({name: "John Doe", age: 40});
+console.log(user.get("name"));
